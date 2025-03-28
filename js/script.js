3eb85123678fb01725a221f71a50768ae5e230dd
unknown <mateus.lorencone@gmail.com>
2025-03-28 13:53:08 -0300

    :hammer: Finalizado a codificação do JavaScript, também foi realizada algumas correções no css.

--- js/script.js
+++ js/script.js
@@ -12,4 +12,27 @@
 
 //Validação de Login
 
+    function login(){
+        var logado = 0;
+        var usuario = document.getElementById("usuario").value;
+        var senha = document.getElementById("senha").value;
+
+        if(usuario == 'admin' && senha == '123456'){
+
+            window.location = "index.html";
+            logado = 1;
+        }
+
+        if(logado == 0){
+
+            alert("Acesso Negado. Usúario ou Senha incorretas")
+        }
+    }
+
 //Ativar alert no botão cadastrar
+
+    function cadastro(){
+
+        alert("Cadastro realizado com sucesso!");
+        window.location.href = "index.html";
+    }
