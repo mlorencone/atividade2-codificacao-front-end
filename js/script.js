4329833d6543e0eb44e0c3595d60e6c2f1798714
unknown <mateus.lorencone@gmail.com>
2025-03-28 13:21:24 -0300

    :construction: Incio da criação do JavaScript que será utilizado no site

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 //efeito do botão voltar ao Topo
+    
+    function topo(){
+        window.scrollTo(
+            {
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+            }
+        )
+    }
 
 //Validação de Login
 
